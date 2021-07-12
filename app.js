4af37dcea727fbefdeb91f0494b0c71301a2f59b
lycho33 <cho.lyds@gmail.com>
2021-07-12 17:07:50 -0400

    all is coded out for the game. must fix matching every pair of cards

--- app.js
+++ app.js
@@ -50,13 +50,62 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/pizza.png'
         },
     ]
-})
+
+
+cardArray.sort(() => 0.5 - Math.random())
 
 const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+const cardsChosen = []
+const cardsChosenId = []
+const cardsWon = []
 
 //create your board
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipcard)
+        grid.appendChild(card)
     }
 }
+
+//check for matches 
+function checkForMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found them all!'
+    }
+}
+
+//flip your card
+function flipcard() {
+    console.log(this)
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500) //invoke function after 500 miliseconds 
+    }
+}
+
+createBoard()
+
+})
